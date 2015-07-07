fe8db7c592d0262f13a4e626e5b02c53952f6cab
Arjan Verkerk <arjan.verkerk@gmail.com>
2015-07-07 11:40:31 +0200

    Completed rebase script.

--- raster_tools/rebase.py
+++ raster_tools/rebase.py
@@ -11,22 +11,79 @@ import logging
 import os
 import sys
 
+import numpy as np
+
 from raster_tools import gdal
-# from raster_tools import gdal_array
 from raster_tools import ogr
-# from raster_tools import osr
+
+from raster_tools import datasets
 
 logger = logging.getLogger(__name__)
 
+DRIVER_GDAL_GTIFF = gdal.GetDriverByName(b'gtiff')
 
-def single(base_path, source_path, target_path):
+
+def single(base_path, source_path, target_path, tolerance=None):
     """ Rebase source on base and write it to target. """
-    logger.debug(base_path)
-    logger.debug(source_path)
-    logger.debug(target_path)
+    # read datasets
+    logger.debug('Read base: {}'.format(base_path))
+    base = gdal.Open(base_path)
+    base_band = base.GetRasterBand(1)
+    base_data = base_band.ReadAsArray()
+    base_mask = ~base_band.GetMaskBand().ReadAsArray().astype('b1')
+
+    logger.debug('Read source: {}'.format(source_path))
+    source = gdal.Open(source_path)
+    source_band = source.GetRasterBand(1)
+    source_data = source_band.ReadAsArray()
+    source_mask = ~source_band.GetMaskBand().ReadAsArray().astype('b1')
+
+    # calculation
+    logger.debug('Determine difference...')
+    try:
+        no_data_value = np.finfo(base_data.dtype).max
+    except ValueError:
+        no_data_value = np.iinfo(base_data.dtype).max
+
+    # give all data the same no_data_value
+    base_data[base_mask] = no_data_value
+    source_data[source_mask] = no_data_value
+
+    # calculate content based on equality or tolerance
+    if tolerance is None:
+        index = np.not_equal(source_data, base_data)
+    else:
+        index = np.greater(np.abs(source_data - base_data), tolerance)
+
+    target_data = np.empty_like(source_data)
+    target_data.fill(no_data_value)
+    target_data[index] = source_data[index]
+
+    # write target
+    logger.debug('Write target: {}'.format(target_path))
+
+    target_dir = os.path.dirname(target_path)
+    try:
+        os.makedirs(target_dir)
+    except OSError:
+        pass
+
+    kwargs = {
+        'projection': base.GetProjection(),
+        'no_data_value': no_data_value.item(),
+        'geo_transform': base.GetGeoTransform(),
+    }
+
+    with datasets.Dataset(target_data[np.newaxis, ...], **kwargs) as dataset:
+        DRIVER_GDAL_GTIFF.CreateCopy(
+            target_path,
+            dataset,
+            options=['compress=deflate'],
+        )
 
 
-def rebase(index_path, base_root, source_root, target_root):
+def rebase(index_path, base_root, source_root, target_root, tolerance):
+    """ Rebase files based on features from shapefile. """
     index = ogr.Open(index_path)
     layer = index[0]
     total = layer.GetFeatureCount()
@@ -37,7 +94,8 @@ def rebase(index_path, base_root, source_root, target_root):
         base_path = os.path.join(base_root, sub, leaf + '.tif')
         source_path = os.path.join(source_root, sub, leaf + '.tif')
         target_path = os.path.join(target_root, sub, leaf + '.tif')
-        single(base_path=base_path,
+        single(tolerance=tolerance,
+               base_path=base_path,
                source_path=source_path,
                target_path=target_path)
         gdal.TermProgress_nocb(count / total)
@@ -50,6 +108,7 @@ def get_parser():
     parser.add_argument('base_root', metavar='BASE')
     parser.add_argument('source_root', metavar='SOURCE')
     parser.add_argument('target_root', metavar='TARGET')
+    parser.add_argument('-t', '--tolerance', type=float)
     return parser
 
 
