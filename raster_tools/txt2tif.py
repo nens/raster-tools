2231e29849f430a38576776c2bf12e4cb8e442bf
Arjan Verkerk <arjan.verkerk@gmail.com>
2016-02-19 14:01:37 +0100

    Dust off txt2tif.

--- raster_tools/txt2tif.py
+++ raster_tools/txt2tif.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-""" 
-Create tif rasters from ZVP points by linear interpolation using griddata.
+"""
+Create tif rasters from xyz files by linear interpolation using griddata.
 """
 
 from __future__ import print_function
@@ -10,43 +10,22 @@ from __future__ import absolute_import
 from __future__ import division
 
 import argparse
-import logging
 import math
 import os
-import sys
-import zipfile
 
-from osgeo import gdal
-from osgeo import osr
 from scipy import interpolate
 import numpy as np
 
 from raster_tools import datasets
+from raster_tools import gdal
+from raster_tools import osr
 
-logger = logging.getLogger(__name__)
-
-WIDTH = 0.25
-HEIGHT = 0.25
+WIDTH = 0.5
+HEIGHT = 0.5
 NO_DATA_VALUE = -9999
-DRIVER = gdal.GetDriverByName(b'gtiff')
-PROJECTION = osr.GetUserInputAsWKT(b'epsg:3043')
-
-
-def read(archive, name):
-    """ Read from zip into points. """
-    logger.debug('Count lines in "{}".'.format(name))
-    total = 100000
-    with archive.open(name) as fobj:
-        total = fobj.read().count('\n')
-    points = np.empty((total, 3), dtype='f4')
-    logger.debug('Reading points from "{}".'.format(name))
-    with archive.open(name) as fobj:
-        for count, line in enumerate(fobj):
-            if count == total:
-                break
-            points[count] = line.split(',')
-            gdal.TermProgress_nocb((count + 1) / total)
-    return points
+DRIVER = gdal.GetDriverByName(str('gtiff'))
+OPTIONS = ['compress=deflate', 'tiled=yes']
+PROJECTION = osr.GetUserInputAsWKT(str('epsg:28992'))
 
 
 def rasterize(points):
@@ -62,22 +41,26 @@ def rasterize(points):
     geo_transform = p, WIDTH, 0, q, 0, -HEIGHT
 
     # simple filling
-    # array = -9999 * np.ones((1, height, width), dtype='f4')
-    # index0 = np.uint32((q - points[:, 1]) / HEIGHT)
-    # index1 = np.uint32((points[:, 0] - p) / WIDTH)
-    # array[0, index0, index1] = points[:, 2]
+    array = -9999 * np.ones((1, height, width), dtype='f4')
+    index0 = np.uint32((q - points[:, 1]) / HEIGHT)
+    index1 = np.uint32((points[:, 0] - p) / WIDTH)
+    array[0, index0, index1] = points[:, 2]
 
     # using interpolation
-    cells = np.indices((height, width)).transpose(1, 2, 0).reshape(-1, 2)
-    rescale = lambda x: (x - (xmin, ymin)) / (xmax - xmin, ymax - ymin)
-    xi = rescale((p, q) + cells[:, ::-1] * (WIDTH, -HEIGHT))
-    pts = rescale(points[:, :2])
-    vals = points[:, 2]
-    array = interpolate.griddata(xi=xi,
-                                 points=pts,
-                                 values=vals,
-                                 fill_value=NO_DATA_VALUE)
-    array.shape = 1, height, width
+    # cells = np.indices((height, width)).transpose(1, 2, 0).reshape(-1, 2)
+    # rescale = lambda x: (x - (xmin, ymin)) / (xmax - xmin, ymax - ymin)
+    # xi = rescale((p, q) + cells[:, ::-1] * (WIDTH, -HEIGHT))
+    # pts = rescale(points[:, :2])
+    # vals = points[:, 2]
+    # array = interpolate.griddata(xi=xi,
+                                 # points=pts,
+                                 # values=vals,
+                                 # fill_value=NO_DATA_VALUE)
+    # array.shape = 1, height, width
+
+    print(len(points))
+    print(array.size)
+    print(len(points) / array.size)
 
     return {'array': array,
             'projection': PROJECTION,
@@ -85,50 +68,25 @@ def rasterize(points):
             'geo_transform': geo_transform}
 
 
-def convert(archive, name):
-    points = read(archive=archive, name=name)
+def command(source_path):
+    root, ext = os.path.splitext(source_path)
+    points = np.loadtxt(source_path)
     kwargs = rasterize(points)
-    path = '{}.tif'.format(os.path.splitext(os.path.basename(name))[0])
-    logger.debug('Saving to "{}".'.format(path))
+    target_path = root + '.tif'
     with datasets.Dataset(**kwargs) as dataset:
-        DRIVER.CreateCopy(path, dataset, options=['compress=deflate'])
-
-
-def is_new(name):
-    path = '{}.tif'.format(os.path.splitext(os.path.basename(name))[0])
-    return not os.path.exists(path)
-
-
-def command(path):
-    with zipfile.ZipFile(path) as archive:
-        for name in sorted(archive.namelist()):
-            if name.endswith('txt') and 'FINAL' not in name and is_new(name):
-                convert(archive=archive, name=name)
+        DRIVER.CreateCopy(target_path, dataset, options=['compress=deflate'])
 
 
 def get_parser():
     """ Return argument parser. """
-    parser = argparse.ArgumentParser(
-        description=__doc__
-    )
-    parser.add_argument(
-        'path',
-        metavar='FILE',
-    )
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('source_path', metavar='FILE')
     return parser
 
 
 def main():
     """ Call command with args from parser. """
-    logging.basicConfig(stream=sys.stderr,
-                        level=logging.DEBUG,
-                        format='%(message)s')
-    try:
-        return command(**vars(get_parser().parse_args()))
-    except SystemExit:
-        raise  # argparse does this
-    except:
-        logger.exception('An exception has occurred.')
+    return command(**vars(get_parser().parse_args()))
 
 
 if __name__ == '__main__':
---
+++ raster_tools/zvp2tif.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+Create tif rasters from ZVP points by linear interpolation using griddata.
+"""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import division
+
+import argparse
+import logging
+import math
+import os
+import sys
+import zipfile
+
+from osgeo import gdal
+from osgeo import osr
+from scipy import interpolate
+import numpy as np
+
+from raster_tools import datasets
+
+logger = logging.getLogger(__name__)
+
+WIDTH = 0.25
+HEIGHT = 0.25
+NO_DATA_VALUE = -9999
+DRIVER = gdal.GetDriverByName(b'gtiff')
+PROJECTION = osr.GetUserInputAsWKT(b'epsg:3043')
+
+
+def read(archive, name):
+    """ Read from zip into points. """
+    logger.debug('Count lines in "{}".'.format(name))
+    total = 100000
+    with archive.open(name) as fobj:
+        total = fobj.read().count('\n')
+    points = np.empty((total, 3), dtype='f4')
+    logger.debug('Reading points from "{}".'.format(name))
+    with archive.open(name) as fobj:
+        for count, line in enumerate(fobj):
+            if count == total:
+                break
+            points[count] = line.split(',')
+            gdal.TermProgress_nocb((count + 1) / total)
+    return points
+
+
+def rasterize(points):
+    """ Create array. """
+    xmin, ymin = points[:, :2].min(0)
+    xmax, ymax = points[:, :2].max(0)
+
+    p = math.floor(xmin / WIDTH) * WIDTH
+    q = math.floor(ymax / HEIGHT) * HEIGHT
+
+    width = int((xmax - p) / WIDTH) + 1
+    height = int((q - ymin) / HEIGHT) + 1
+    geo_transform = p, WIDTH, 0, q, 0, -HEIGHT
+
+    # simple filling
+    # array = -9999 * np.ones((1, height, width), dtype='f4')
+    # index0 = np.uint32((q - points[:, 1]) / HEIGHT)
+    # index1 = np.uint32((points[:, 0] - p) / WIDTH)
+    # array[0, index0, index1] = points[:, 2]
+
+    # using interpolation
+    cells = np.indices((height, width)).transpose(1, 2, 0).reshape(-1, 2)
+    rescale = lambda x: (x - (xmin, ymin)) / (xmax - xmin, ymax - ymin)
+    xi = rescale((p, q) + cells[:, ::-1] * (WIDTH, -HEIGHT))
+    pts = rescale(points[:, :2])
+    vals = points[:, 2]
+    array = interpolate.griddata(xi=xi,
+                                 points=pts,
+                                 values=vals,
+                                 fill_value=NO_DATA_VALUE)
+    array.shape = 1, height, width
+
+    return {'array': array,
+            'projection': PROJECTION,
+            'no_data_value': NO_DATA_VALUE,
+            'geo_transform': geo_transform}
+
+
+def convert(archive, name):
+    points = read(archive=archive, name=name)
+    kwargs = rasterize(points)
+    path = '{}.tif'.format(os.path.splitext(os.path.basename(name))[0])
+    logger.debug('Saving to "{}".'.format(path))
+    with datasets.Dataset(**kwargs) as dataset:
+        DRIVER.CreateCopy(path, dataset, options=['compress=deflate'])
+
+
+def is_new(name):
+    path = '{}.tif'.format(os.path.splitext(os.path.basename(name))[0])
+    return not os.path.exists(path)
+
+
+def command(path):
+    with zipfile.ZipFile(path) as archive:
+        for name in sorted(archive.namelist()):
+            if name.endswith('txt') and 'FINAL' not in name and is_new(name):
+                convert(archive=archive, name=name)
+
+
+def get_parser():
+    """ Return argument parser. """
+    parser = argparse.ArgumentParser(
+        description=__doc__
+    )
+    parser.add_argument(
+        'path',
+        metavar='FILE',
+    )
+    return parser
+
+
+def main():
+    """ Call command with args from parser. """
+    logging.basicConfig(stream=sys.stderr,
+                        level=logging.DEBUG,
+                        format='%(message)s')
+    try:
+        return command(**vars(get_parser().parse_args()))
+    except SystemExit:
+        raise  # argparse does this
+    except:
+        logger.exception('An exception has occurred.')
+
+
+if __name__ == '__main__':
+    exit(main())
