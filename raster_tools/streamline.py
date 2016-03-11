b223aeab6bb76dacec753f4d50faacaa7848c66a
Arjan Verkerk <arjan.verkerk@gmail.com>
2016-03-11 17:21:14 +0100

    Tackled depression filling.

--- raster_tools/streamline.py
+++ raster_tools/streamline.py
@@ -28,7 +28,7 @@ FLOW_ARRAY = np.array([(64, 128, 1),
 
 FLOW_INDICES = FLOW_ARRAY.nonzero()
 FLOW_NUMBERS = FLOW_ARRAY[FLOW_INDICES][np.newaxis, ...]
-FLOW_OFFSETS = np.array(FLOW_INDICES).reshape(1, 8, 2) - 1
+FLOW_OFFSETS = np.array(FLOW_INDICES).transpose()[np.newaxis] - 1
 FLOW_INVERSE = FLOW_ARRAY[tuple(-np.array(FLOW_OFFSETS[0].T) + 1)][np.newaxis]
 
 # FLOW_VECTORS = None  # later, to get rid of trigonometry
@@ -80,6 +80,81 @@ def fill_simple_depressions(values):
     edge = ndimage.minimum_filter(values, footprint=footprint)
     locs = edge > values
     values[locs] = edge[locs]
+
+
+def fill_complex_depressions(values, mask=None):
+    """
+    Fill complex depressions in a bottom-up approach, roughly analogous to:
+
+    http://topotools.cr.usgs.gov/pdfs/
+    methods_applications_surface_depression_analysis.pdf
+
+    :param values: DEM values
+    :param mask: cells defined as not-in-a-depression
+    """
+    # start with edges marked as not-in-a-depression
+    if mask is None:
+        mask = np.zeros(values.shape, dtype='b1')
+        mask[0, :-1] = True
+        mask[:-1, -1] = True
+        mask[-1, 1:] = True
+        mask[1:, 0] = True
+
+    # structure allows for diagonal flow
+    structure = np.ones((3, 3))
+
+    while not mask.all():
+
+        # iterate to find outer countours of depressions
+        while True:
+            edges = ndimage.binary_dilation(mask, structure=structure) - mask
+
+            # find edge values
+            t_index1 = edges.nonzero()
+            values1 = values[t_index1][:, np.newaxis]
+
+            # find neighbour values
+            t_index8 = get_neighbours(t_index1)
+            values8 = values[t_index8].reshape(-1, 8)
+
+            # neighbours that are lower than edge values
+            b_index8a = values8 <= values1
+            # neighbours that are marked as not-in-a-depression
+            b_index8b = mask[t_index8].reshape(-1, 8)
+            # combined index into edge values to set to true
+            b_index8 = np.logical_and.reduce([b_index8a, b_index8b])
+
+            if not b_index8.any():
+                break
+
+            # adjust mask
+            mask[t_index1] = b_index8.any(axis=1)
+
+            # print((~mask).sum())
+
+        if mask.all():
+            return
+
+        # include edges of undefined, this may merge adjacent depressions
+        dilated = ndimage.binary_dilation(~mask, structure=structure)
+        # label these depressions
+        label_total, count = ndimage.label(dilated, structure=structure)
+        # label their edges as well
+        label_edges = (dilated - ~mask) * label_total
+        # measure the minimum of the edge per depression
+        label_minima = ndimage.minimum(
+            values,
+            labels=label_edges,
+            index=range(1, count + 1),
+        )
+        # make a mapping array that links label to edge minima
+        label_mapper = np.hstack([
+            np.finfo(label_minima.dtype).min, label_minima,
+        ])
+        # fill depressions up to these minima
+        nonzero = label_total.nonzero()
+        values[nonzero] = np.maximum(values[nonzero],
+                                     label_mapper[label_total[nonzero]])
 
 
 def calculate_flow_direction(values):
@@ -136,18 +211,18 @@ def calculate_flow_direction(values):
         edges = undefined - ndimage.binary_erosion(undefined)
 
         t_index1 = edges.nonzero()
-        direction1 = direction[t_index1]
+        direction1 = direction[t_index1][:, np.newaxis]
 
-        # find neigh
+        # find neighbour values
         t_index8 = get_neighbours(t_index1)
         direction8 = direction[t_index8].reshape(-1, 8)
 
         # neighbour must be in encoded direction
-        b_index8a = np.bool8(direction1[:, np.newaxis] & FLOW_NUMBERS)
+        b_index8a = np.bool8(direction1 & FLOW_NUMBERS)
         # neighbour must have a defined flow direction
         b_index8b = ~np.bool8(np.log2(direction8))
         # that direction must not point towards the cell to be defined
-        b_index8c = direction1[:, np.newaxis] != FLOW_INVERSE
+        b_index8c = direction1 != FLOW_INVERSE
         # combined index
         b_index8 = np.logical_and.reduce([b_index8a, b_index8b, b_index8c])
 
@@ -163,20 +238,11 @@ def calculate_flow_direction(values):
     return np.where(np.log2(direction) % 1, 0, direction)
 
 
-def fill_complex_depressions(direction, values):
-    """ Return watershed, the rest remains to be solved later. """
-    label, count = ndimage.label(direction == 0)
-    hi, lo = values.max(), values.min()
-    unsigned = ((values - lo / (hi - lo)) * 65535).astype('u2')
-    return ndimage.watershed_ift(unsigned, markers=label)
-
-
 def calculate_flow_accumulation(direction):
     # start with any defined direction as 0 and the rest as maxint
     # add 1 to any cell pointed to, or first uniq -c it.
     # any cell updated is basis for next step.
-    import ipdb
-    ipdb.set_trace()
+    pass
 
 
 class Streamliner(object):
@@ -211,7 +277,7 @@ class Streamliner(object):
         except OSError:
             pass  # no problem
 
-        geometry = index_feature.geometry().Buffer(-400)
+        geometry = index_feature.geometry().Buffer(0)
         geo_transform = self.geo_transform.shifted(geometry)
 
         # data
@@ -221,9 +287,11 @@ class Streamliner(object):
 
         # processing
         fill_simple_depressions(values)
+        fill_complex_depressions(values)
         direction = calculate_flow_direction(values)
-        accumulation = calculate_flow_accumulation(direction)
-        values = accumulation
+        direction
+        # accumulation = calculate_flow_accumulation(direction)
+        # values = accumulation
 
         # save
         values = values[np.newaxis]
