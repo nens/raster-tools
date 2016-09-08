ff20bfaf0b58f1e3096c89408eebb24541e69664
Arjan Verkerk <arjan.verkerk@gmail.com>
2016-09-08 14:51:28 +0200

    In principle, a working zonal.

--- raster_tools/zonal.py
+++ raster_tools/zonal.py
@@ -18,15 +18,15 @@ import argparse
 import logging
 import math
 import re
-import sys
 
 import numpy as np
 
 from raster_tools import gdal
-from raster_tools import ogr
 
-from raster_tools import groups
 from raster_tools import utils
+from raster_tools import groups
+from raster_tools import datasets
+from raster_tools import data_sources
 
 logger = logging.getLogger(__name__)
 
@@ -55,33 +55,11 @@ def get_kwargs(geometry):
         return {'width': width, 'height': height}
 
 
-def mask(dataset, geometry):
-    """ Clip dataset in place to geometry is into dataset. """
-    # determine inverse geometry
-    x1, x2, y1, y2 = geometry.GetEnvelope()
-    wkt = POLYGON.format(x1=x1, y1=y1, x2=x2, y2=y2)
-    sr = geometry.GetSpatialReference()
-    box = ogr.CreateGeometryFromWkt(wkt, sr)
-    mask = box.Difference(geometry)
-
-    # put geometry into temporary layer
-    datasource = DRIVER_OGR_MEMORY.CreateDataSource('')
-    layer = datasource.CreateLayer(str(''), sr)
-    layer_defn = layer.GetLayerDefn()
-    feature = ogr.Feature(layer_defn)
-    feature.SetGeometry(mask)
-    layer.CreateFeature(feature)
-
-    # burn no data
-    burn_values = [dataset.GetRasterBand(1).GetNoDataValue()]
-    gdal.RasterizeLayer(dataset, [1], layer, burn_values=burn_values)
-
-
 class Analyzer(object):
     """
     A container that does the computation per feature.
     """
-    def __init__(self, output_path, raster_paths, statistics):
+    def __init__(self, raster_paths, statistics):
         # raster group
         self.group = groups.Group(*map(gdal.Open, raster_paths))
 
@@ -91,98 +69,91 @@ class Analyzer(object):
         pattern = re.compile('(p)([0-9]+)')
         for statistic in statistics:
             # allow for different column name
-            try:
+            if ':' in statistic:
                 column, statistic = statistic.split(':')
-            except ValueError:
+            else:
                 column = statistic
 
             # determine the action
             match = pattern.match(statistic)
             if pattern.match(statistic):
                 percentile = int(match.groups()[1])
-                actions[column] = 'percentile', [percentile]
-            elif statistic == 'value':
-                self.actions[column] = 'item', []
+                self.actions[column] = 'percentile', [percentile]
             else:
                 self.actions[column] = statistic, []
 
         # keep convenient group properties available
         self.geo_transform = self.group.geo_transform
+        self.no_data_value = self.group.no_data_value
 
+        # these kwargs are constant for the whole group
         self.kwargs = {'projection': self.group.projection,
                        'no_data_value': self.group.no_data_value}
 
-    def analyze(self, feature)
-        """ Return attributes to write to the result. """
-        geometry = feature.geometry()
-        array = self.group.read(geometry)
-        geo_transform = self.group.geo_transform.shift(geometry)
+    def read(self, geometry):
+        """
+        Return 2-tuple (array, index)
 
-        kwargs = {'geo_transform' = self.geo_transform.shift(geometry)}
+        The 'array' array is already a linear array of elements within
+        geometry. The The 'data' array is a boolean array indicating
+        wether the corresponding element from 'array' contains data,
+        i.e., whose value does not correspond to the no_data_value.
+        """
+        # determine kwargs to use with GDAL datasets
+        kwargs = {'geo_transform': self.geo_transform.shift(geometry)}
         kwargs.update(self.kwargs)
 
-        mask = np.zeros(array.shape, dtype='u1')
+        # read the data for array
+        array_2d = self.group.read(geometry)
 
+        # prepare a mask to select elements that are within geometry
+        select = np.zeros(array_2d.shape, dtype='u1')
         with data_sources.Layer(geometry) as layer:
-            with dataset.Dataset(mask, **kwargs) as dataset:
+            with datasets.Dataset(select[np.newaxis], **kwargs) as dataset:
                 gdal.RasterizeLayer(dataset, [1], layer, burn_values=[1])
 
-        mask_appropriate
+        # select those elements
+        array = array_2d[select.astype('b1')]
 
-   
-                
+        # determine data or no data
+        if array_2d.dtype.kind == 'f':
+            data = ~np.isclose(array, self.no_data_value)
+        else:
+            data = ~np.equal(array, self.no_data_value)
 
-            utils.burn(dataset=dataset, geometry=geometry)
-            'no_data_value': self.group.no_data_value,
-            'projection': = self.group.projection
+        return array, data
 
-
-
-        
-
-        
-        # get array from group
-
-
-
-
-def command(shape_path, raster_paths, output_path, statistics, part):
-    """ Main """
-    # shape with source features
-    shape = utils.PartialDataSource(shape_path)
-    if part is not None:
-        index = index.select(part)
-
-
-    target = utils.TargetDataSource(
-        path=output_path,
-        template_path=shape_path,
-        attributes=actions,
-    )
-
-    for source_feature in source_features:
+    def analyze(self, feature):
+        """ Return attributes to write to the result. """
         # retrieve raster data
-        geometry = source_feature.geometry()
-        kwargs = get_kwargs(geometry)
-        data = store.get_data(geometry, **kwargs)
-        masked = np.ma.masked_equal(data['values'],
-                                    data['no_data_value'])
-        compressed = masked.compressed()
+        geometry = feature.geometry()
+        array, data = self.read(geometry)
 
         # apppend statistics
-        attributes = source_feature.items()
-        for column, (action, args) in actions.items():
-            try:
-                if hasattr(np.ma, action):
-                    value = getattr(np.ma, action)(masked, *args)
-                else:
-                    value = getattr(np, action)(compressed, *args)
-                value = np.nan if np.ma.is_masked(value) else value
-            except (ValueError, IndexError):
-                value = np.nan
-            attributes[column] = value
+        attributes = feature.items()
+        for column, (action, args) in self.actions.items():
+            attributes[column] = getattr(np, action)(array, *args)
 
-        target.append(geometry=geometry, attributes=attributes)
+        return {'geometry': geometry, 'attributes': attributes}
+
+
+def command(source_path, target_path, raster_paths, statistics, part):
+    """ Main """
+    # open source datasource
+    source = utils.PartialDataSource(source_path)
+    if part is not None:
+        source = source.select(part)
+
+    analyzer = Analyzer(statistics=statistics,
+                        raster_paths=raster_paths)
+
+    # create target datasource
+    target = utils.TargetDataSource(path=target_path,
+                                    template_path=source_path,
+                                    attributes=analyzer.actions)
+
+    for feature in source:
+        target.append(**analyzer.analyze(feature))
     return 0
 
 
